8ed3c56ad4eeefd5da5d05628733f8a383fd0456
m3gofriends <m3gofriends@gmail.com>
2021-08-02 18:17:47 +0800

    Initial commit

--- hello-world/hello_world.go
+++ hello-world/hello_world.go
@@ -1,11 +1,7 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package greeting should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package greeting implements a simple message output.
 package greeting
 
-// HelloWorld should have a comment documenting it.
+// HelloWorld print a message.
 func HelloWorld() string {
 	return "Hello, World!"
 }
