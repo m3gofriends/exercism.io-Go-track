6c91c01270031186d68fda0607f8f7dcad0219ce
m3gofriends <m3gofriends@gmail.com>
2021-08-03 13:23:30 +0800

    Latest release

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -4,15 +4,14 @@ package hamming
 import "errors"
 
 // Distance calculates hamming distance.
-func Distance(a, b string) (int, error) {
+func Distance(a, b string) (distance int, err error) {
 	if len(a) != len(b) {
 		return -1, errors.New("sequence's length are different")
 	}
-	distance := 0
 	for i := 0; i < len(a); i++ {
 		if a[i] != b[i] {
 			distance++
 		}
 	}
-	return distance, nil
+	return distance, err
 }
