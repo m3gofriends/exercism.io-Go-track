073bf124b36ba22d7821e69866c558327a285994
m3gofriends <m3gofriends@gmail.com>
2021-08-12 21:00:49 +0800

    Latest release

--- Core exercises/isogram/isogram.go
+++ Core exercises/isogram/isogram.go
@@ -1,14 +1,21 @@
 // Package isogram checks if string has duplicate letters.
 package isogram
 
-import "strings"
-
 // IsIsogram checks if string has duplicate letters.
 func IsIsogram(s string) bool {
-	s = strings.ToLower(s)
-	for _, value := range s {
-		if (value >= 'a' && value <= 'z') && (strings.Count(s, string(value)) > 1) {
-			return false
+	var letterFlags uint32 = 0
+
+	for i := 0; i < len(s); i++ {
+		letter := s[i]
+		if letter > 96 && letter < 123 {
+			letter -= 32
+		}
+
+		if letter > 64 && letter < 91 {
+			if (letterFlags & (1 << (letter - 'A'))) != 0 {
+				return false
+			}
+			letterFlags |= (1 << (letter - 'A'))
 		}
 	}
 	return true
