01c47720cd5c63b2c5a7f98ff50f5d4aaf099414
m3gofriends <m3gofriends@gmail.com>
2021-08-12 21:32:12 +0800

    Latest release

--- Extra exercises/Easy/pangram/pangram.go
+++ Extra exercises/Easy/pangram/pangram.go
@@ -7,11 +7,11 @@ func IsPangram(s string) bool {
 
 	for i := 0; i < len(s); i++ {
 		letter := s[i]
-		if letter > 64 && letter < 91 {
+		if letter > 64 && letter < 91 { // A to Z
 			letter += 32
 		}
 
-		if letter > 96 && letter < 123 {
+		if letter > 96 && letter < 123 { // a to z
 			letterFlags |= (1 << (letter - 'a'))
 		}
 	}
