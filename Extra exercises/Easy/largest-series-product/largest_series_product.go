9761ce55053e18fe40b117124e1b8239e5b95e8a
m3gofriends <m3gofriends@gmail.com>
2021-08-17 21:49:01 +0800

    Initial commit

---
+++ Extra exercises/Easy/largest-series-product/largest_series_product.go
@@ -0,0 +1,28 @@
+// Package lsproduct calculates the largest product for a contiguous substring of digits of length n.
+package lsproduct
+
+import "errors"
+
+// LargestSeriesProduct calculates the largest product for a contiguous substring of digits of length n.
+func LargestSeriesProduct(s string, span int) (int64, error) {
+	if len(s) < span {
+		return -1, errors.New("span must be smaller than string length")
+	} else if span < 0 {
+		return -1, errors.New("span must be greater than zero")
+	}
+
+	var nowValue, product int64 = 1, 0
+	for i, times := 0, len(s)-span+1; i < times; i, nowValue = i+1, 1 {
+		for j := 0; j < span; j++ {
+			if s[i+j] < 48 || s[i+j] > 57 { // Not equal to 0-9.
+				return -1, errors.New("digits input must only contain digits")
+			}
+			nowValue *= int64(s[i+j] - 48)
+		}
+		if nowValue > product {
+			product = nowValue
+		}
+	}
+
+	return product, nil
+}
