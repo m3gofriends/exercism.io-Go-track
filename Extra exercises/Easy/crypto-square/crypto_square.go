3c1338fba56ce3a8e0d4d7688ff88c71d6d576d8
m3gofriends <m3gofriends@gmail.com>
2021-08-17 16:10:26 +0800

    Initial commit

---
+++ Extra exercises/Easy/crypto-square/crypto_square.go
@@ -0,0 +1,53 @@
+// Package cryptosquare implements the classic method for composing secret messages.
+package cryptosquare
+
+import (
+	"math"
+	"strings"
+)
+
+// Encode implements the classic method for composing secret messages.
+func Encode(s string) string {
+	if len(s) < 2 {
+		return s
+	}
+
+	var c, r, floor, ceil int
+
+	s = strings.ToLower(s)
+	for i := len(s) - 1; i > -1; i-- {
+		if (s[i] < 97 || s[i] > 122) && (s[i] < 48 || s[i] > 57) { // Not equal to a-z and 0-9.
+			s = s[:i] + s[i+1:]
+		}
+	}
+
+	floor = int(math.Floor(math.Sqrt(float64(len(s)))))
+	ceil = int(math.Ceil(math.Sqrt(float64(len(s)))))
+
+	switch length := len(s); {
+	case floor*floor >= length:
+		c, r = floor, floor
+	case (floor+1)*floor >= length:
+		c, r = floor+1, floor
+	case ceil*ceil >= length:
+		c, r = ceil, ceil
+	case (ceil+1)*ceil >= length:
+		c, r = ceil, ceil
+	}
+
+	return string(normalize(s, c, r))
+}
+
+func normalize(s string, c, r int) (output []byte) {
+	for i := 0; i < c; i++ {
+		for index := i; index < c*r; index += c {
+			if index < len(s) {
+				output = append(output, s[index])
+			} else {
+				output = append(output, 32)
+			}
+		}
+		output = append(output, 32)
+	}
+	return output[:len(output)-1]
+}
