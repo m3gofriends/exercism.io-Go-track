ab2ac92e65b363dbf27f48bbdcca90991ae6625d
m3gofriends <m3gofriends@gmail.com>
2021-08-19 14:16:38 +0800

    Initial commit

---
+++ Extra exercises/Medium/pythagorean-triplet/pythagorean_triplet.go
@@ -0,0 +1,55 @@
+// Package pythagorean finds the Pythagorean triplets.
+package pythagorean
+
+import "sort"
+
+// Triplet represents a Pythagorean triplet.
+type Triplet [3]int
+
+var pythagoreanTriple []Triplet = generatePythagorean(426)
+
+// Range returns the list of Pythagorean triplets with sides in the range min to max inclusive.
+func Range(min, max int) (output []Triplet) {
+	for i := 0; i < len(pythagoreanTriple); i++ {
+		if pythagoreanTriple[i][0] >= min && pythagoreanTriple[i][2] <= max {
+			output = append(output, pythagoreanTriple[i])
+		}
+	}
+
+	return output
+}
+
+// Sum returns a list of all Pythagorean triplets where the sum a+b+c is equal to p.
+func Sum(p int) (output []Triplet) {
+	for i := 0; i < len(pythagoreanTriple); i++ {
+		if pythagoreanTriple[i][0]+pythagoreanTriple[i][1]+pythagoreanTriple[i][2] == p {
+			output = append(output, pythagoreanTriple[i])
+		}
+	}
+
+	return output
+}
+
+func generatePythagorean(n int) (pythagoreanList []Triplet) {
+	pythagoreanMap := make(map[Triplet]bool)
+
+	for i, j := 1, 2; j*j+i*i < n; j++ {
+		for k := 1; k*(j*j+i*i) < n; k++ {
+			value := Triplet{k * (j*j - i*i), k * (2 * j), k * (j*j + i*i)}.sort() // k*(j^2-i^2), k*(2*i*j), k*(j^2+i^2)
+			if _, ok := pythagoreanMap[value]; !ok {
+				pythagoreanMap[value] = true
+				pythagoreanList = append(pythagoreanList, value)
+			}
+		}
+	}
+
+	sort.Slice(pythagoreanList, func(i, j int) bool { return pythagoreanList[i][0] < pythagoreanList[j][0] })
+	return pythagoreanList
+}
+
+func (t Triplet) sort() Triplet {
+	if t[0] > t[1] {
+		t[0], t[1] = t[1], t[0]
+	}
+	return t
+}
